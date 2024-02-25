e2c10339cdc7ef38eaf53d0e36379cd7dee87b30
maryem <maryemby21@gmail.com>
2024-02-25 22:07:19 +0100

    fixed the message

--- src/exercice1/ClientUDP.java
+++ src/exercice1/ClientUDP.java
@@ -10,30 +10,38 @@ public class ClientUDP {
         try {
             // on récupère les paramètres : nom de la machine serveur et
             // numéro de port
-            System.out.println("Adress du serveur : ");
+            System.out.println("Adresse du serveur : ");
             host = keyb.next();
             System.out.println("port d'écoute du serveur : ");
             port = keyb.nextInt();
             InetAddress adr;
             DatagramPacket packet;
             DatagramSocket socket;
+
             // adr contient l'@IP de la partie serveur
             adr = InetAddress.getByName(host);
+
             // données à envoyer : chaîne de caractères
-            byte[] data = (new String("Hello Word")).getBytes();
+            byte[] data = (new String("Hello Server")).getBytes();
+
             // création du paquet avec les données et en précisant l'adresse
             // du serveur (@IP et port sur lequel il écoute)
             packet = new DatagramPacket(data, data.length, adr, port);
+
             // création d'une socket, sans la lier à un port particulier
             socket = new DatagramSocket();
+
             // envoi du paquet via la socket
             socket.send(packet);
+
             // création d'un tableau vide pour la réception
             byte[] reponse = new byte[15];
             packet.setData(reponse);
             packet.setLength(reponse.length);
+
             // attente paquet envoyé sur la socket du client
             socket.receive(packet);
+
             // récupération et affichage de la donnée contenue dans le paquet
             String chaine = new String(packet.getData(), 0,
                     packet.getLength());
